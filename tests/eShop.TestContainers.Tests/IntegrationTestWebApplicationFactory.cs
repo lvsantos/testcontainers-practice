12d58474139e419db3cfc30a47e1344fb2e36fd4
Lucas Valtudes <lvaltudes@symplicity.com>
2024-07-08 17:50:52 -0300

    feat: Criar testes de integração para validar uso do sql server

---
+++ tests/eShop.TestContainers.Tests/IntegrationTestWebApplicationFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.MsSql;
+
+namespace eShop.TestContainers.Tests;
+
+public abstract class IntegrationTestWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    protected MsSqlContainer _dbContainer;
+
+    public IntegrationTestWebApplicationFactory()
+    {
+        _dbContainer = new MsSqlBuilder()
+            .Build();
+    }
+
+    public Task InitializeAsync() => _dbContainer.StartAsync();
+    Task IAsyncLifetime.DisposeAsync() => _dbContainer.DisposeAsync().AsTask();
+}
---
+++ tests/eShop.TestContainers.Tests/MsSqlServerContainerTest .cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace eShop.TestContainers.Tests;
+
+public class MsSqlServerContainerTest : IntegrationTestWebApplicationFactory
+{
+    [Fact]
+    public async Task ReadFromMsSqlDatabase()
+    {
+        await using var connection = new SqlConnection(_dbContainer.GetConnectionString());
+        await connection.OpenAsync();
+
+        await using var command = connection.CreateCommand();
+        command.CommandText = "SELECT 1;";
+
+        var actual = await command.ExecuteScalarAsync() as int?;
+        Assert.Equal(1, actual.GetValueOrDefault());
+    }
+}
