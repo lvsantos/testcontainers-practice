2e035ef8d07f865f2ca2e119b61e3098bbf9fc67
Lucas Valtudes <lucassantosdesenvolvedor@gmail.com>
2024-07-08 18:12:56 -0300

    feat: Criar testes de integração para validar escrita no sql server

--- tests/eShop.TestContainers.Tests/MsSqlServerContainerTest .cs
+++ tests/eShop.TestContainers.Tests/MsSqlServerContainerTest .cs
@@ -16,4 +16,23 @@ public class MsSqlServerContainerTest : IntegrationTestWebApplicationFactory
         var actual = await command.ExecuteScalarAsync() as int?;
         Assert.Equal(1, actual.GetValueOrDefault());
     }
+    [Fact]
+    public async Task WriteToMsSqlDatabase()
+    {
+        await using var connection = new SqlConnection(_dbContainer.GetConnectionString());
+        await connection.OpenAsync();
+
+        await using var command = connection.CreateCommand();
+
+        command.CommandText = "CREATE TABLE TestTable (Id INT);";
+        await command.ExecuteNonQueryAsync();
+
+        command.CommandText = "INSERT INTO TestTable (Id) VALUES (10);";
+        await command.ExecuteNonQueryAsync();
+
+        command.CommandText = "SELECT Id FROM TestTable;";
+        var actual = await command.ExecuteScalarAsync() as int?;
+
+        Assert.Equal(10, actual);
+    }
 }
